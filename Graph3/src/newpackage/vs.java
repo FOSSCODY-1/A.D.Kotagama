1c601478f103841788be5870b9da097fac64fb7e
AselaDK <38081311+AselaDK@users.noreply.github.com>
2018-09-26 07:28:05 +0530

    interfaces are created and connected

---
+++ Graph3/src/newpackage/vs.java
@@ -0,0 +1,20 @@
+package newpackage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class vs {
+    
+    private static int vertex;
+    
+    public  void setVertex(int ver){
+        vertex = ver;
+    }
+    
+    public  int  getVertex() {
+        return vertex;
+    }
+    
+}
